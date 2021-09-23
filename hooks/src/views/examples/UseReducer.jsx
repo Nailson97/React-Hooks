38a36c63525fab105f0d2895b4182d03b72a33db
Nailson97 <nailsonsousa97@gmail.com>
2021-09-23 19:20:17 -0300

    inicio estudo useReducer

--- hooks/src/views/examples/UseReducer.jsx
+++ hooks/src/views/examples/UseReducer.jsx
@@ -1,13 +1,43 @@
-import React from 'react'
+import React, { useReducer } from 'react'
 import PageTitle from '../../components/layout/PageTitle'
 
+const initialState = {
+    card: [],
+    products: [],
+    user: null,
+    number: 0
+}
+
+function reducer(state, action) {
+    switch(action.type) {
+     case 'add2ToNumber':
+         return {...state, number: state.number + 2}   
+     case 'login':
+        return {...state, user: {name: action.payload}}    
+     default:
+         return state    
+    }
+}
+
 const UseReducer = (props) => {
+    const [state, dispach] = useReducer(reducer, initialState )
     return (
         <div className="UseReducer">
             <PageTitle
                 title="Hook UseReducer"
-                subtitle="Uma outra forma de ter estado em componentes funcionais!"
-            />
+                subtitle="Uma outra forma de ter estado em componentes funcionais!"/>
+
+                <div className="center">
+                    {state.user ?<span className="text">{state.user.name}</span>
+                    : <span className="text">Sem usúario</span>
+                    }
+                   
+                    <span className="text">{state.number}</span>
+                    <div>
+                        <button className="btn" onClick={() => dispach({type: 'login', payload: 'Maria'})}>Login</button>
+                        <button className="btn" onClick={() => dispach({type: 'add2ToNumber'})}> +2</button>
+                    </div>
+                </div>
         </div>
     )
 }
